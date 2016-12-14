8d17ea21abecad581298cae3f7af645b78440f85
Obi-Bram Keno Beer <gizmo_xxl@hotmail.com>
2016-12-14 20:51:36 +0100

    add callback function to show badges in page

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,19 @@
-$(function() {
+$(function () {
 
-  // your code will go here
+    $.ajax({
+        url: 'https://www.codeschool.com/users/bramlaurijsen.json',
+        dataType: 'jsonp',
+        success: function (response) {
+            let badge, badges = response.courses.completed;
+            let filltemplate, template = '<div class="course"><h3>%title%</h3><img src="%image%" alt="%title% badge"><a class="btn btn-primary" href="%url%" target="_blank">See course</a></div>';
+            let container = $('#badges');
+            for (i=0; i < badges.length; i++){
+                badge = badges[i];
+                filltemplate = template.replace(/%title%/g, badge.title).replace('%image%', badge.badge).replace('%url%', badge.url);
+                container.append(filltemplate);
+                // to check json object of each badge in the console, use: console.log(badge);
+            }
+        }
+    });
 
 });
